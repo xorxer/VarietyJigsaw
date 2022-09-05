a0dc9ef557f2e70670dca466ba6c5e1b5917a150
AntLe12 <le672869@gmail.com>
2022-09-04 21:54:35 -0700

    Created a grid for the jigsaw puzzle and defined a basic structure for a Piece class that eases drawing on a canvas.

--- frontend/src/Piece.js
+++ frontend/src/Piece.js
@@ -1,4 +1,17 @@
 class Piece
 {
-    
+    constructor(rowInd, colInd)
+    {
+        this.rowInd = rowInd;
+        this.colInd = colInd;
+    }
+
+    // Assigns a section of the image to a piece
+    // as well as draw it on the canvas
+    draw(context)
+    {
+        
+    }
 }
+
+export default Piece
