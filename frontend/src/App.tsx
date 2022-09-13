69e2f0d41bcf74c9890890646ddff6b8798e9ef7
AntLe12 <le672869@gmail.com>
2022-09-12 23:56:35 -0700

    Initial condition to check for locking a piece. Current bug: first piece getting locked in is never in the correct position.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -124,18 +124,23 @@ const App = () => {
       if(currPiece != null)
       {
             // Overshooting can be determined via a threshhold using the pythagorean theorem
-            // if()
-            // {
+            if(getDistance(currPiece.x, currPiece.y, currPiece.getStartX(), currPiece.getStartY()) < Math.min(currPiece.widthSize, currPiece.heightSize) / 3)
+            {
                 const index = pieces.indexOf(currPiece);
                 pieces.splice(index, 1);
                 pieces.push(currPiece);
                 currPiece.canMove = false;
                 currPiece.x = currPiece.getStartX();
                 currPiece.y = currPiece.getStartY();
-                // }
+                setLockedPieceCount(lockedPieceCount + 1);
+                drawCanvas();
+            }
         }
         currPiece = null;
-        drawCanvas();
+  }
+
+  const getDistance = (x1: number, y1: number, x2: number, y2: number) => {
+        return Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
   }
 
   const onMouseMove = (e: MouseEvent) => {
