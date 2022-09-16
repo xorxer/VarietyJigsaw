2a43e731a130204022af45063c63106004b38b63
AntLe12 <le672869@gmail.com>
2022-09-15 21:59:09 -0700

    Added font for menu, and fixed structure for menu.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -186,16 +186,23 @@ const App = () => {
     <div>
       <canvas id='canvas' width={WIDTH} height={HEIGHT}></canvas>
       <div className='menu'>
+            <label>Difficulty</label>
+            <br></br>
             <select>
-                Difficulty
                 <option value='easy'>Easy</option>
                 <option value='medium'>Medium</option>
                 <option value='hard'>Hard</option>
                 <option value='extreme'>Extreme</option>
             </select>
             <ul className='nav'>
-                <li className='btn'>Grid</li>
-                <li className='btn'>Background</li>
+                <li className='btn'>
+                    Grid
+                    <input type="checkbox" id="switch" /><label htmlFor="switch">Toggle</label>
+                </li>
+                <li className='btn'>
+                    Background
+                    <input type="checkbox" id="switch" /><label htmlFor="switch">Toggle</label>
+                </li>
                 <li className='btn'>Play</li>
             </ul>
         </div>
